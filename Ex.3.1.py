2971898d6c5639f038bca062d6ac572d983404d7
shreyashajare24 <shreyashajare10@gmail.com>
2026-08-24 13:25:10 +0530

    Create Ex.3.1.py

---
+++ Ex.3.1.py
@@ -0,0 +1,8 @@
+
+age = int(input("Enter your age: "))
+income = float(input("Enter annual family income: "))
+
+if age < 25 and income < 300000:
+    print("Eligible for Scholarship")
+else:
+    print("Not Eligible for Scholarship")
