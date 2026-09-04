0de6d19fd20f6670d1ced8798acfcca9692c8c78
shreyashajare24 <shreyashajare10@gmail.com>
2026-09-04 13:46:13 +0530

    Create Ex.9.2.py

---
+++ Ex.9.2.py
@@ -0,0 +1,10 @@
+grades = [75, 82, 68, 90, 85]
+
+print("Original Grades:", grades)
+
+index = int(input("Enter index position: "))
+new_grade = int(input("Enter new grade: "))
+
+grades[index] = new_grade
+
+print("Corrected Grades:", grades)
